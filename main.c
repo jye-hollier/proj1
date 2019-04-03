8afe4cb2e56dcf7d5e25cbc5e2a4fe6c784e1772
Jye Hollier <jye.hollier@outlook.com>
2019-04-03 00:45:42 +0000

    Start of code and outlines of functions

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
 int main()  {
-
+    char originalText[] = "abcd gf\0hjh";
+    
+    for(i = 0; i <= /*string legth*/; i++)
+    printf("%s", originalText);
   return 0;
+}
+
+// Function to convert to all capitals// 97-122
+for(i = 0; i <= /*string legth*/; i++)  {
+    if(/*ASCII code*/ >=97 && /*ASCII code*/ <= 122) {
+        /*ASCII code*/ = /*ASCII code*/ - 32
+    }
+}
+
+// Function to determine rotation
+for(i = 0; i <= /*string legth*/; i++)  {
+    //use a switch to add up different letters then determine most common
+    //then do rotation function
+    //then do spell check function
+}
+
+
+// Function to change character in rotation cipher
+
+for(i = 0; i <= /*string legth*/; i++)  {
+    if(/*ASCII code*/ >= 41 && /*ASCII code*/ <= 90 && ...)    {   //if code is a capital letter AND code plus rotation amount is still in range
+        /*ASCII code*/ = /*ASCII code*/ + rotationNumber
+    }
+    else if (//its a capital and it plus rotation goes out of range) {  //BUT if this takes it outside of the range it must then be reduced by 26?
+        code = code + change - 26
+    }
+
+        
+        
+    }
 }
